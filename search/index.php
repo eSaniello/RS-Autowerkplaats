aebd8e2bc8bb5aa21ae457fdfe044324c3b5c82f
eSaniello <shaniel29samadhan@gmail.com>
2019-07-04 22:03:34 -0300

    formatted code

--- search/index.php
+++ search/index.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <script type="text/javascript">
         function myFunction() {
-        // Declare variables 
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
-        tr = table.getElementsByTagName("tr");
+            // Declare variables 
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
 
-        // Loop through all table rows, and hide those who don't match the search query
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            td2 = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-            txtValue = td.textContent || td.innerText;
-            txtValue2 = td2.textContent || td2.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[0];
+                td2 = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    txtValue2 = td2.textContent || td2.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
             }
-            } 
-        }
         }
     </script>
 </head>
+
 <body>
     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search..">
     <br><br>
@@ -53,4 +55,5 @@
         </tr>
     </table>
 </body>
+
 </html>
