4d0b5d4ab3c7aeb9641ede62cfca9d185df5fa18
eSaniello <shaniel29samadhan@gmail.com>
2019-07-04 22:00:30 -0300

    formatted the code

--- src/add_registratie/addklant.php
+++ src/add_registratie/addklant.php
@@ -2,21 +2,19 @@
 // Include config file
 require_once "../incl/config.php";
 
-            $naam = mysqli_real_escape_string($link, $_POST['naam']);
-            $voornaam = mysqli_real_escape_string($link, $_POST['voornaam']);
-            $adres = mysqli_real_escape_string($link, $_POST['adres']);
-            $mobiel = mysqli_real_escape_string($link, $_POST['mobiel']);    
+$naam = mysqli_real_escape_string($link, $_POST['naam']);
+$voornaam = mysqli_real_escape_string($link, $_POST['voornaam']);
+$adres = mysqli_real_escape_string($link, $_POST['adres']);
+$mobiel = mysqli_real_escape_string($link, $_POST['mobiel']);
 
-                $sql = "INSERT INTO klant(naam, voornaam, mobiel, adres)
+$sql = "INSERT INTO klant(naam, voornaam, mobiel, adres)
             VALUES ('$naam', '$voornaam', '$mobiel', '$adres')";
 
-            if ($link->query($sql) === TRUE) {
-                header("Location: ../index.php");
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $link->error;
-            }
+if ($link->query($sql) === TRUE) {
+    header("Location: ../index.php");
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
 
-            $link->close();
-
-?>
+$link->close();
--- src/add_registratie/addvoertuig.php
+++ src/add_registratie/addvoertuig.php
@@ -2,29 +2,27 @@
 // Include config file
 require_once "../incl/config.php";
 
-            $merk = mysqli_real_escape_string($link, $_POST['merk']);
-            $model = mysqli_real_escape_string($link, $_POST['model']);
-            $categorie = mysqli_real_escape_string($link, $_POST['categorie']);
-            $bouwjaar = mysqli_real_escape_string($link, $_POST['bouwjaar']);
-            $kenteken_nr = mysqli_real_escape_string($link, $_POST['kenteken_nr']);
-            $chassis_nr = mysqli_real_escape_string($link, $_POST['chassis_nr']);
-            $keuring_vervaldatum = mysqli_real_escape_string($link, $_POST['keuring_vervaldatum']);
-            $klantnaam = mysqli_real_escape_string($link, $_POST['naam']);
-            $klantvoornaam = mysqli_real_escape_string($link, $_POST['voornaam']);
+$merk = mysqli_real_escape_string($link, $_POST['merk']);
+$model = mysqli_real_escape_string($link, $_POST['model']);
+$categorie = mysqli_real_escape_string($link, $_POST['categorie']);
+$bouwjaar = mysqli_real_escape_string($link, $_POST['bouwjaar']);
+$kenteken_nr = mysqli_real_escape_string($link, $_POST['kenteken_nr']);
+$chassis_nr = mysqli_real_escape_string($link, $_POST['chassis_nr']);
+$keuring_vervaldatum = mysqli_real_escape_string($link, $_POST['keuring_vervaldatum']);
+$klantnaam = mysqli_real_escape_string($link, $_POST['naam']);
+$klantvoornaam = mysqli_real_escape_string($link, $_POST['voornaam']);
 
-            $klant_id = query("SELECT klant_id FROM klant where naam == $klantnaam && voornaam == $klantvoornaam");
-            
-            
-                $sql = "INSERT INTO voertuig(merk, model, bouwjaar, kenteken_nr, chassis_nr, categorie, keuring_vervaldatum)
+$klant_id = query("SELECT klant_id FROM klant where naam == $klantnaam && voornaam == $klantvoornaam");
+
+
+$sql = "INSERT INTO voertuig(merk, model, bouwjaar, kenteken_nr, chassis_nr, categorie, keuring_vervaldatum)
             VALUES ('$merk', '$model', '$bouwjaar', '$kenteken_nr','$chassis_nr', '$categorie', $keuring_vervaldatum)";
 
-            if ($link->query($sql) === TRUE) {
-                header("Location: ../index.php");
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $link->error;
-            }
+if ($link->query($sql) === TRUE) {
+    header("Location: ../index.php");
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
 
-            $link->close();
-
-?>
+$link->close();
