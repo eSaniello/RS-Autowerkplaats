17636e4efbcdf7e71e743d7ca50535a0c60d5bc2
eSaniello <shaniel29samadhan@gmail.com>
2019-07-18 14:38:09 -0300

    Lil fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>RS Autowerkplaats</title>
-  <!-- Google icons -->
+  <!-- Google Icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <!-- materialize -->
