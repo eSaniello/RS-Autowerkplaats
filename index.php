f21ac09bc6a0e83d4cb4905e4037cf6f6342e87d
pebloa <kenson.latchmansing.natin@gmail.com>
2019-07-18 20:20:29 -0300

    made changes to search funtion

--- index.php
+++ index.php
@@ -1,149 +1,148 @@
 <!DOCTYPE html>
 <html>
-
 <head>
 	<title>Autowerkplaats</title>
-	<!--Import Google Icon Font-->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Anton|Merriweather|Open+Sans+Condensed:300|Roboto+Condensed" rel="stylesheet">
-	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="materialize/css/materialize.css" media="screen,projection" />
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<!-- Import javascript file -->
-	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+	 <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Anton|Merriweather|Open+Sans+Condensed:300|Roboto+Condensed" rel="stylesheet">
+    <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- Import javascript file -->
+        <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 </head>
-
 <body>
-	<p>tabel wegsleep: afhaal_datum = null</p>
+    <p>tabel wegsleep: afhaal_datum = null</p>
 	<!-- KEURING MODAL -->
 	<div id="modal2" class="modal" style="height: 600px; width: 1000px;">
-		<div class="modal-content">
-			<h3>Keuring Verlengen</h3>
-			<br><br>
-			<div class="row">
-				<form class="col s12" action="incl/keuring.php" method="post">
-					<div class="row">
-						<div class="input-field col s6">
-							<select name="voertuig" required>
-								<?php
-								$date = date('y-m-d');
-								include 'incl/db.php';
-								$sql = "SELECT * FROM voertuig WHERE keuring_vervaldatum <= '$date' ORDER BY kenteken_nr ASC";
-								$result = mysqli_query($link, $sql);
+     	<div class="modal-content">
+     		<h3>Keuring Verlengen</h3>
+     		<br><br>
+     		<div class="row">
+     			<form class="col s12" action="incl/keuring.php" method="post">
+		      		<div class="row">
+		      			<div class="input-field col s6">
+		      				<select name="voertuig" required>
+			                    <?php
+                                    $date = date('y-m-d');
+				                    include 'incl/db.php';
+				                    $sql = "SELECT * FROM voertuig WHERE keuring_vervaldatum <= '$date' ORDER BY kenteken_nr ASC";
+				                    $result = mysqli_query($link, $sql);
 
-								while ($row = mysqli_fetch_array($result)) {
-									echo "<option value=" . $row['voertuig_id'] . ">" . $row['merk'] . " " . $row['model'] . "	| " . $row['kenteken_nr'] . "    | " . $row['categorie'] . "</option>";
-								}
-								?>
-							</select>
-							<label>Voertuig</label>
-						</div>
-						<div class="input-field col s6">
-							<input name="nieuw-keuring" type="text" class="datepicker" required>
-							<label for="nieuw-keuring">Nieuw keuring vervaldatum</label>
-						</div>
-					</div>
-					<div class="row">
-						<div class="input-field col s6">
-							<button class="blue darken-4 waves-effect waves-light btn modal-trigger" href="#modal1">Voertuig registreren</button>
-							<button class="blue darken-4 waves-effect waves-light btn" name="submit">Opslaan</button>
-						</div>
-					</div>
-				</form>
-			</div>
-			<div class="row">
-				<table class="tarief">
-					<thead>
-						<th> </th>
-						<th>P1</th>
-						<th>P2</th>
-						<th>P3</th>
-						<th>P4</th>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Tarief</td>
-							<td>SRD 50</td>
-							<td>SRD 100</td>
-							<td>SRD 150</td>
-							<td>SRD 200</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
+				                    while($row = mysqli_fetch_array($result)) {
+				                    	echo "<option value=". $row['voertuig_id'] . ">" . $row['merk'] . " " . $row['model'] . "	| " . $row['kenteken_nr'] . "    | " . $row['categorie'] . "</option>";
+				                    }
+			                    ?>
+			                 </select>
+		                  	<label>Voertuig</label> 
+		      			</div>
+		      			<div class="input-field col s6">
+		      				<input name="nieuw-keuring" type="text" class="datepicker" required>
+		      				<label for="nieuw-keuring">Nieuw keuring vervaldatum</label>
+		      			</div>
+		      		</div>
+		      		<div class="row">
+		      			<div class="input-field col s6">
+			      			<button class="blue darken-4 waves-effect waves-light btn modal-trigger" href="#modal1">Voertuig registreren</button>
+			      			<button class="blue darken-4 waves-effect waves-light btn" name="submit">Opslaan</button>
+		      			</div>
+		      		</div>
+		      	</form>
+		    </div>
+            <div class="row">
+                <table class="tarief">
+                    <thead>
+                        <th> </th>
+                        <th>P1</th>
+                        <th>P2</th>
+                        <th>P3</th>
+                        <th>P4</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarief</td>
+                            <td>SRD 50</td>
+                            <td>SRD 100</td>
+                            <td>SRD 150</td>
+                            <td>SRD 200</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+	    </div>
+    </div>
 
-	<!-- REPARATIE MODAL -->
-	<div id="modal1" class="modal" style="height: 600px; width: 1000px;">
-		<div class="modal-content">
-			<h3>Reparatie</h3>
-			<br><br>
-		</div>
-	</div>
+    <!-- REPARATIE MODAL -->
+    <div id="modal1" class="modal" style="height: 600px; width: 1000px;">
+    	<div class="modal-content">
+    		<h3>Reparatie</h3>
+    		<br><br>
+    	</div>
+    </div>
 
-	<!-- WEGSLEPEN MODAL -->
-	<div id="modal3" class="modal" style="height: 600px; width: 1000px;">
-		<div class="modal-content">
-			<h3>Wegslepen</h3>
-			<br><br>
-			<div class="row">
-				<form class="col s12" action="incl/wegslepen.php" method="post">
-					<div class="row">
-						<div class="input-field col s6">
-							<input type="text" name="merk" required>
-							<label for="merk">Merk</label>
-						</div>
-						<div class="input-field col s6">
-							<input type="text" name="model" required>
-							<label for="model">Model</label>
-						</div>
-					</div>
-					<div class="row">
-						<div class="input-field col s6">
-							<label for="kentekennummer">Kentekennummer</label>
-							<input type="text" name="kentekennummer">
-						</div>
-						<div class="input-field col s6">
-							<input type="number" name="afstand" required>
-							<label for="afstand">Afstand (Km)</label>
-						</div>
-					</div>
-					<div class="row">
-						<div class="input-field col s6">
-							<button class="blue darken-4 waves-effect waves-light btn" name="submit">Opslaan</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+    <!-- WEGSLEPEN MODAL -->
+    <div id="modal3" class="modal" style="height: 600px; width: 1000px;">
+    	<div class="modal-content">
+    		<h3>Wegslepen</h3>
+    		<br><br>
+    		<div class="row">
+    			<form class="col s12" action="incl/wegslepen.php" method="post">
+    				<div class="row">
+    					<div class="input-field col s6">
+    						<input type="text" name="merk" required>
+    						<label for="merk">Merk</label>
+    					</div>
+    					<div class="input-field col s6">
+    						<input type="text" name="model" required>
+    						<label for="model">Model</label>
+    					</div>
+    				</div>
+    				<div class="row">
+    					<div class="input-field col s6">
+    						<label for="kentekennummer">Kentekennummer</label>
+    						<input type="text" name="kentekennummer">
+    					</div>
+                        <div class="input-field col s6">
+                            <input type="number" name="afstand" required>
+                            <label for="afstand">Afstand (Km)</label>
 
-	<!-- FLOATING BUTTON -->
-	<div class="fixed-action-btn">
+                        </div>
+    				</div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <button class="blue darken-4 waves-effect waves-light btn" name="submit">Opslaan</button>
+                        </div>
+                    </div>
+    			</form>
+    		</div>
+    	</div>
+    </div>
+
+    <!-- FLOATING BUTTON -->
+    <div class="fixed-action-btn">
 		<a class="btn-floating btn-large yellow darken-2">
-			<i class="large material-icons">add</i>
-		</a>
+	    	<i class="large material-icons">add</i>
+	 	</a>
 		<ul>
-			<li><a class="btn-floating red darken-1 modal-trigger" href="#modal3"><i class="material-icons"><i class="material-icons"><img src="img/tow-truck.png" style="height: 18px; width: 26px;"></i></a></li>
-			<li><a class="btn-floating blue darken-4 modal-trigger" href="#modal2"><i class="material-icons"><img src="img/car-icon.png" style="height: 24px; width: 24px;"></i></a></li>
-			<li><a class="btn-floating yellow darken-2 modal-trigger" href="#modal1"><i class="material-icons"><img src="img/wrench.png" style="height: 24px; width: 24px;"></i></a></li>
+		    <li><a class="btn-floating red darken-1 modal-trigger" href="#modal3"><i class="material-icons"><i class="material-icons"><img src="img/tow-truck.png" style="height: 18px; width: 26px;"></i></a></li>
+		    <li><a class="btn-floating blue darken-4 modal-trigger" href="#modal2"><i class="material-icons"><img src="img/car-icon.png" style="height: 24px; width: 24px;"></i></a></li>
+		    <li><a class="btn-floating yellow darken-2 modal-trigger" href="#modal1"><i class="material-icons"><img src="img/wrench.png" style="height: 24px; width: 24px;"></i></a></li>
 		</ul>
 	</div>
 
 	<!-- JAVASCRIPT -->
-	<script type="text/javascript">
-		M.AutoInit();
+    <script type="text/javascript">
+    	M.AutoInit();
 
-		document.addEventListener('DOMContentLoaded', function() {
-			var elems = document.querySelectorAll('.fixed-action-btn');
-			var instances = M.FloatingActionButton.init(elems, {
-				direction: 'top',
-				hoverEnabled: false
-			});
+    	document.addEventListener('DOMContentLoaded', function() {
+		    var elems = document.querySelectorAll('.fixed-action-btn');
+			    var instances = M.FloatingActionButton.init(elems, {
+			    	direction: 'top',
+			    	hoverEnabled: false
+			    });
 		});
-	</script>
-</body>
 
+    </script>
+</body>
 </html>
--- search/index.php
+++ search/index.php
@@ -3,22 +3,32 @@
 
 <head>
     <script type="text/javascript">
-        function myFunction() {
+        function searchFunction() {
             // Declare variables 
             var input, filter, table, tr, td, i, txtValue;
-            input = document.getElementById("myInput");
+            //ID VAN JE SEARCHBAR
+            input = document.getElementById("search");
             filter = input.value.toUpperCase();
-            table = document.getElementById("myTable");
+            //ID VAN JE REPARATIE TABEL
+            table = document.getElementById("reparatieTabel");
             tr = table.getElementsByTagName("tr");
 
             // Loop through all table rows, and hide those who don't match the search query
             for (i = 0; i < tr.length; i++) {
+                //merk
                 td = tr[i].getElementsByTagName("td")[0];
+                //model
                 td2 = tr[i].getElementsByTagName("td")[1];
+                //bouwjaar
+                td3 = tr[i].getElementsByTagName("td")[2];
+                //kentekennr
+                td4 = tr[i].getElementsByTagName("td")[3];
                 if (td) {
                     txtValue = td.textContent || td.innerText;
                     txtValue2 = td2.textContent || td2.innerText;
-                    if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+                    txtValue3 = td3.textContent || td3.innerText;
+                    txtValue4 = td4.textContent || td4.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1 || txtValue3.toUpperCase().indexOf(filter) > -1 || txtValue4.toUpperCase().indexOf(filter) > -1) {
                         tr[i].style.display = "";
                     } else {
                         tr[i].style.display = "none";
@@ -30,28 +40,38 @@
 </head>
 
 <body>
-    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search..">
+    <input type="search" id="search" onkeyup="searchFunction()" placeholder="Zoeken..">
     <br><br>
-    <table id="myTable">
+    <table id="reparatieTabel">
         <tr>
-            <th>naam</th>
-            <th>land</th>
+            <th>merk</th>
+            <th>model</th>
+            <th>bouwjaar</th>
+            <th>kentekennr</th>
         </tr>
         <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Germany</td>
+            <td>toyota</td>
+            <td>ractis</td>
+            <td>2013</td>
+            <td>8757</td>
         </tr>
         <tr>
-            <td>Berglunds snabbkop</td>
-            <td>Sweden</td>
+            <td>toyota</td>
+            <td>mark-x</td>
+            <td>2015</td>
+            <td>8757</td>
         </tr>
         <tr>
-            <td>Island Trading</td>
-            <td>UK</td>
+            <td>kia</td>
+            <td>optima</td>
+            <td>2016</td>
+            <td>9869</td>
         </tr>
         <tr>
-            <td>Koniglich Essen</td>
-            <td>Germany</td>
+            <td>nissan</td>
+            <td>skyline</td>
+            <td>2005</td>
+            <td>7648</td>
         </tr>
     </table>
 </body>
